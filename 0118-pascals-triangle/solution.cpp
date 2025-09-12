5803bc4819845b3d174116db9e734e11c04da87a
Anshika Agarwal <anshikaagarwal2802@gmail.com>
2025-09-12 18:53:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.8 MB (9.58%)

---
+++ 0118-pascals-triangle/solution.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    vector<vector<int>> generate(int numRows) 
+    {
+        
+    
+         vector< vector<int> >ans1;
+         for (int i = 1; i <= numRows; i++)
+        {
+            long long  ans = 1;
+            vector<int> ansrow;
+            ansrow.push_back(1);
+               for (int col = 1; col<i; col++)
+               {
+                          ans =  ans * (i- col);
+                          ans = ans / (col);
+                          ansrow.push_back(ans);
+               }
+        
+               ans1.push_back(ansrow);
+        }
+        return ans1;
+        
+        
+        
+    }
+   
+};
+
