b919e8f82a22b720f536de83691bee2cf07946bc
Anshika Agarwal <anshikaagarwal2802@gmail.com>
2026-01-09 18:34:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.8 MB (12.10%)

--- 0031-next-permutation/solution.cpp
+++ 0031-next-permutation/solution.cpp
@@ -1,38 +1,7 @@
 class Solution {
 public:
-   void nextPermutation(vector<int>& nums) {
-        int n=nums.size(),i,indx=-1;
-
-        for(i=n-2;i>=0;i--)
-        {
-            if(nums[i]<nums[i+1])
-            {
-                indx=i;
-                break;
-            }
-        }
-        if(indx==-1)
-        {
-            reverse(nums.begin(),nums.end());
-            return;
-            
-            
-        }
-        for(i=n-1;i>indx;i--)
-        {
-            if(nums[i]>nums[indx])
-            {
-                swap(nums[i],nums[indx]);
-                break;
-
-            }
-        }
-        reverse(nums.begin()+indx+1,nums.end());
-        return;
-
-
-        
-        
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
 
 
 
