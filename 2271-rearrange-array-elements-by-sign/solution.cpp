4d66bdb8dc24b019adf80d074d59bc5edbe6b0ef
Anshika Agarwal <anshikaagarwal2802@gmail.com>
2026-01-09 17:01:06 +0000

    Sync LeetCode submission Runtime - 13 ms (15.86%), Memory - 128.3 MB (54.80%)

--- 2271-rearrange-array-elements-by-sign/solution.cpp
+++ 2271-rearrange-array-elements-by-sign/solution.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-    vector<int> rearrangeArray(vector<int>& nums)
-    {
+    vector<int> rearrangeArray(vector<int>& nums) {
         int n=nums.size();
         vector<int>ans(n,0);
         int posindex=0,negindex=1;
-        
         for(int i=0;i<n;i++)
         {
             if(nums[i]>0)
@@ -16,14 +14,14 @@ public:
             }
             else
             {
-               ans[negindex]=nums[i];
-               negindex+=2;
+                ans[negindex]=nums[i];
+                negindex+=2;
 
             }
 
-            
 
-        }
-        return ans;
+        }  
+        return ans ;
+              
     }
 };
