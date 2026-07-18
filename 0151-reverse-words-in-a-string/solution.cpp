482afc4181abac5564e44d89bd37d41b7dccf5de
Anshika Agarwal <anshikaagarwal2802@gmail.com>
2026-07-18 13:40:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (95.15%)

--- 0151-reverse-words-in-a-string/solution.cpp
+++ 0151-reverse-words-in-a-string/solution.cpp
@@ -1,45 +1,50 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        
-        string ans="";
-        int i=s.size()-1;
-        while(i>=0)
+        int right = 0;                              // reading
+        int left = 0;                               // writing
+        while (right < s.size() && s[right] == ' ') // remove all leading spaces
         {
+            right++;
+        }
+        while (right < s.size()) {
+            if (s[right] != ' ') {
+                s[left] = s[right];
+                left++;
 
-          while(i>=0 && s[i]==' ')
-        {
-            i--;
+            } else {
+                if (left > 0 &&
+                    s[left - 1] != ' ') // as if left-> 0 then s[-1] is invalid
+                {
+
+                    s[left] = ' ';
+                    left++;
+                }
+            }
+            right++;
+        }
+        // removing trailing spaces
+        if (s[left - 1] == ' ') {
+            left--;
+        }
+        s.resize(left);
+        reverse(s.begin(), s.end());
+        int i = 0;
+        while (i < s.size()) {
+            int start = i;
+            while (i < s.size() && s[i] != ' ') {
+                i++;
+            }
+            int end = i - 1;
+            while (start < end) {
+                swap(s[start], s[end]);
+                start++;
+                end--;
+            }
+            i++;
 
         }
-        if(i<0)
-        {
-            break;
-
-        }
-        int end=i;
-        while(i>=0 && s[i]!=' ')
-        {
-               i--;
-        }
-        int start=i+1;
-        string word=s.substr(start,end-start+1);
-        if(!ans.empty())
-        {
-            ans+=" ";
-
-        }
-        ans+=word;
-
-        }
-        
-
-    return ans ;
-
-
-
-
-
-        
+        return s ;
     }
-};
+        
+    };
