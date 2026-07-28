e9c46f6d1eb132efa885e7b98684037e857acba1
Anshika Agarwal <anshikaagarwal2802@gmail.com>
2026-07-28 06:08:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (88.58%)

---
+++ 0812-rotate-string/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        string temp=s+s;
+        if(s.size()!=goal.size())return false;
+        // if(temp.find(goal)!=string::npos)
+        // {
+        //     return true ;
+
+        // }
+
+        // return false;
+        return (temp.find(goal)!=string::npos);
+        
+
+
+        
+
+        
+    }
+};
