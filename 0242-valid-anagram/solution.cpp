41ed4bfb0aa5fa8924ae6ce9dc99019a58f0df15
Anshika Agarwal <anshikaagarwal2802@gmail.com>
2026-07-28 11:03:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (26.27%)

---
+++ 0242-valid-anagram/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int>mpp;
+        for(int i=0;i<s.size();i++)
+        {
+            mpp[s[i]]++;
+
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            if(mpp[t[i]]==0)
+            return false;
+            mpp[t[i]]--;
+
+
+        }
+        return true ;
+        
+
+
+        
+
+    }
+};
