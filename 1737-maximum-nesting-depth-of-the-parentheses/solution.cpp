771c2d98acd503af76c15ec026a1ebd4cd3bab8f
Anshika Agarwal <anshikaagarwal2802@gmail.com>
2026-08-01 20:15:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.3 MB (61.49%)

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int depth=0,maxdepth=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+            {
+                depth++;
+                maxdepth=max(maxdepth,depth);
+
+
+            }
+            else if(s[i]==')')
+            {
+                depth--;
+
+            }
+        }
+        return maxdepth;
+
+
+
+                                           
+
+        
+    }
+};
